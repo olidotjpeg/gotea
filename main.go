87f8a384ea22c557bd8a8d6946d0b40e5ebb43f6
Oliver Munkebo <oliver.munkebo@instana.com>
2022-05-23 08:51:58 +0200

    added uuid, support for teanames, and SPA supports

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"github.com/google/uuid"
-	"github.com/gorilla/mux"
+	"os"
+	"path/filepath"
+	"time"
 )
+
 
 type Origin struct {
 	ShopName     string `json:"shopName"`
@@ -23,6 +27,8 @@ type Tea struct {
 	ContainerWeight int `json:"containerWeight"`
 	InitialWeight   int `json:"initialWeight"`
 	BrewingDuration int `json:"brewingDuration"`
+	TeaType string `json:"teaType"`
+	TeaName string `json:"teaName"`
 }
 
 var Teas []Tea
@@ -40,6 +46,8 @@ func generateTeas() {
 			ContainerWeight: 95,
 			InitialWeight:   100,
 			BrewingDuration: 120,
+			TeaType: "Green Tea",
+			TeaName: "Organic Sencha Arata",
 		},
 		Tea{
 			Id: uuid.NewString(),
@@ -52,12 +60,10 @@ func generateTeas() {
 			ContainerWeight: 35,
 			InitialWeight:   120,
 			BrewingDuration: 1000,
+			TeaType: "Green Tea",
+			TeaName: "AC Afternoon Tea",
 		},
 	}
-}
-
-func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Welcome to the HomePage!")
 }
 
 func getTeas(w http.ResponseWriter, r *http.Request){
@@ -104,6 +110,7 @@ func updateTea(w http.ResponseWriter, r *http.Request) {
 			tea.BrewingDuration = updatedTea.BrewingDuration
 			tea.Origin.ShopLocation = updatedTea.Origin.ShopLocation
 			tea.Origin.ShopName = updatedTea.Origin.ShopName
+			tea.TeaName = updatedTea.TeaName
 
 			Teas[i] = tea
 
@@ -125,9 +132,51 @@ func deleteTea(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Teas)
 }
 
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/teas", getTeas)
 	// NOTE: Ordering is important here! This has to be defined before
 	// the other `/tea` endpoint.
@@ -135,7 +184,21 @@ func handleRequests() {
 	myRouter.HandleFunc("/tea/{id}", deleteTea).Methods("DELETE")
 	myRouter.HandleFunc("/tea/{id}", updateTea).Methods("PUT")
 	myRouter.HandleFunc("/tea/{id}", returnSingleTea)
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
+
+
+	spa := spaHandler{staticPath: "static", indexPath: "index.html"}
+	myRouter.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler:      myRouter,
+		Addr:         "0.0.0.0:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+
+	log.Fatal(srv.ListenAndServe())
 }
 
 func main() {

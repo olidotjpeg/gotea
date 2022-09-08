0c0c8f05c6d1310ce898bdceac1ad242445fb8e3
Oliver Munkebo <oliver.munkebo@instana.com>
2022-09-08 15:53:30 +0200

    chore: updated handlers code to support cors

--- handlers.go
+++ handlers.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 var Database *sql.DB
@@ -21,11 +22,16 @@ func HandleRequests() {
 	myRouter.HandleFunc("/tea/{id}", updateTea).Methods(http.MethodPut, http.MethodOptions)
 	myRouter.HandleFunc("/tea/{id}", returnSingleTea).Methods(http.MethodGet)
 
-	// Handling cors
-	myRouter.Use(mux.CORSMethodMiddleware(myRouter))
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost", "http://localhost:5173"},
+		AllowedHeaders: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "DELETE", "PUT"},
+		// Enable Debugging for testing, consider disabling in production
+		Debug: true,
+	})
 
 	srv := &http.Server{
-		Handler: myRouter,
+		Handler: c.Handler(myRouter),
 		Addr:    "0.0.0.0:8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
